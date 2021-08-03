2a4d8b5b2d0157c0f5132549e87b0680f9cf8228
Martin Maiolo <35736622+martinmaiolo@users.noreply.github.com>
2021-08-03 00:09:10 -0300

    Create Organization + horas.js

---
+++ Organization + horas.js
@@ -0,0 +1,50 @@
+<script type="application/ld+json">
+{
+    "@type": "Organization",
+    "@id": "https://superadmin.es/#organization",
+    "name": "Superadmin",
+    "legalName": "Superadmin, S.L.",
+    "url": "https://superadmin.es/",
+    "contactPoint": [
+        {
+            "@type": "ContactPoint",
+            "@id": "https://superadmin.es/#contactpoint",
+            "telephone": "+34625222225",
+            "hoursAvailable": [
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "opens": "09:00:00",
+                    "closes": "20:00:00"
+                }
+            ],
+            "contactType": "sales",
+            "availableLanguage": [
+                {
+                    "@type": "Language",
+                    "name": "Spanish"
+                }
+            ]
+        }
+    ],
+    "sameAs": [
+        "https://www.facebook.com/soysuperadmin/",
+        "https://www.instagram.com/soysuperadmin/",
+        "https://twitter.com/soysuperadmin/"
+    ],
+    "logo": {
+        "@type": "ImageObject",
+        "@id": "https://superadmin.es/#logo",
+        "url": "https://medios.superadmin.es/superadmin/superadmin-640x120.jpg",
+        "width": 640,
+        "height": 120,
+        "caption": "Superadmin"
+    },
+    "image": {
+        "@type": "ImageObject",
+        "@id": "https://superadmin.es/#image",
+        "url": "https://superadmin.es/img/superadmin-hosting-wordpress-administrado.jpg",
+        "height": 1080,
+        "width": 1920
+    }
+}
+</script>
